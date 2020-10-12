06c1fd545ef397ddb2fded9698be90a0f0056ec7
bobstrange <bob.1983.g@gmail.com>
2020-10-13 00:04:31 +0900

    fix db:seeds syntax error

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+author = Author.create(
+  name: 'example'
+)
+post = Post.create(
+  title: 'Vue3',
+  body: 'Vue (pronounced /vjuː/, like view) is a progressive framework for building user interfaces. Unlike other monolithic frameworks, Vue is designed from the ground up to be incrementally adoptable.',
+  author_id: author.id
+)
+post = Post.create(
+  title: 'Next.js',
+  body: 'Next.js gives you the best developer experience with all the features you need for production: hybrid static & server rendering, TypeScript support, smart bundling, route pre-fetching, and more. No config needed.',
+  author_id: author.id
+)
+
+post = Post.create(
+  title: 'React',
+  body: 'React has been designed from the start for gradual adoption, and you can use as little or as much React as you need.',
+  author_id: author.id
+)
+
