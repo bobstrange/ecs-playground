0c16aa7628ee829e3dd1b3772367962c802dc332
bobstrange <bob.1983.g@gmail.com>
2020-10-18 23:59:34 +0900

    fix response type

--- front/src/apis/posts.ts
+++ front/src/apis/posts.ts
@@ -1,15 +1,30 @@
 import { client } from '@/apis/client'
 
-export type Post = {
-  id: number;
-  title: string;
-  body: string;
-  author_id: number;
-  likesCount: number;
+type PostResponse = {
+  id: number
+  title: string
+  body: string
+  author_id: number
+  likes_count: number
+}
+
+export type Post = Omit<Omit<PostResponse, 'likes_count'>, 'author_id'> & {
+  likesCount: number
+  authorId: number
+}
+
+const postResponseMapper = (res: PostResponse) => {
+  return { ...res, likesCount: res.likes_count || 0, authorId: res.author_id }
 }
 
 export async function fetchPosts(): Promise<Post[]> {
-  return (await client.get('posts')).data.map((post: any) => {
-    return { ...post, likesCount: post.likes_count || 0 }
-  })
+  return (await client.get<PostResponse[]>('posts')).data.map(
+    postResponseMapper
+  )
+}
+
+export async function incrementLike(id: Post['id']) {
+  return (
+    await client.post<PostResponse[]>(`posts/${id}/increment_like`)
+  ).data.map(postResponseMapper)
 }
