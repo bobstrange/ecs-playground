5599a90f3541021013da6a1e6ff83cc95d8efe5f
bobstrange <bob.1983.g@gmail.com>
2020-10-18 21:58:09 +0900

    Fix shims-vue.d.ts

--- front/src/shims-vue.d.ts
+++ front/src/shims-vue.d.ts
@@ -1,9 +1,5 @@
-// declare module '*.vue' {
-//   import type { DefineComponent } from 'vue'
-//   const component: DefineComponent
-//   export default component
-// }
 declare module '*.vue' {
-  import Vue from 'vue'
-  export default Vue
+  import type { defineComponent } from 'vue'
+  const Component: defineComponent
+  export default Component
 }
