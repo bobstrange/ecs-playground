8098f82ed0f3e6a8ffb23f1370c7ef2b9eb9b71a
bobstrange <bob.1983.g@gmail.com>
2020-10-19 23:54:04 +0900

    added subnet configs

--- infra/main.tf
+++ infra/main.tf
@@ -14,3 +14,66 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_subnet" "public_1a" {
+  vpc_id = aws_vpc.main.id
+
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.0.1.0/24"
+  tags = {
+    Name = "ecs-sandbox-1a"
+  }
+}
+
+resource "aws_subnet" "public_1c" {
+  vpc_id = aws_vpc.main.id
+
+  availability_zone = "ap-northeast-1c"
+  cidr_block        = "10.0.2.0/24"
+  tags = {
+    Name = "ecs-sandbox-1c"
+  }
+}
+
+resource "aws_subnet" "public_1d" {
+  vpc_id = aws_vpc.main.id
+
+  availability_zone = "ap-northeast-1d"
+  cidr_block        = "10.0.3.0/24"
+  tags = {
+    Name = "ecs-sandbox-1d"
+  }
+}
+
+resource "aws_subnet" "private_1a" {
+  vpc_id = aws_vpc.main.id
+
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.0.10.0/24"
+
+  tags = {
+    Name = "ecs-sandbox-private-1a"
+  }
+}
+
+resource "aws_subnet" "private_1c" {
+  vpc_id = aws_vpc.main.id
+
+  availability_zone = "ap-northeast-1c"
+  cidr_block        = "10.0.20.0/24"
+
+  tags = {
+    Name = "ecs-sandbox-private-1c"
+  }
+}
+
+resource "aws_subnet" "private_1d" {
+  vpc_id = aws_vpc.main.id
+
+  availability_zone = "ap-northeast-1d"
+  cidr_block        = "10.0.20.0/24"
+
+  tags = {
+    Name = "ecs-sandbox-private-1d"
+  }
+}
+
